581069f3872cf706b975e33326f35bb228afc65e
henzoparahua <hnz.2894henzop19@gmail.com>
2025-05-10 17:00:16 -0300

    ops, just forgot hinstance on main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "header/systemclass.h"
 
-int WINAPI WinMain(HINSTANCE, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
 {
 	SystemClass* System;
 	bool result;
