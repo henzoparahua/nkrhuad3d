6b20a2d72684cd997f7731886e12d0a6eb23077a
henzoparahua <hnz.2894henzop19@gmail.com>
2025-07-11 01:59:17 -0300

    The fundamental difference between `WinMain` and `wWinMain` lies in how they handle text strings, particularly command-line arguments. This distinction is rooted in the broader concept of ANSI vs. Unicode character encodings in Windows programming.
    The `w` from `wWinMain` comes from “wide character version”, introduced for native Unicode (16-bit, UTF-16) support. It is highly preferred for all modern Windows applications to use `wWinMain` function, because Windows itself is a Unicode-based operating system since Windows NT. All internal string handling and file system operations use Unicode.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include "header/systemclass.h"
 
-// WinMain is the conventional name for the application entry point.
-// It initializes the app, displays its main window, and enters a message
-// retrieval-and-dispatch loop—the top-level control structure for the
-// remainder of the application's execution.
-// When the WM_QUIT message is received, the message loop terminates and the application ends.
-int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, PSTR pScmdline, int iCmdshow)
+int WINAPI wWinMain(HINSTANCE hinstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int iCmdshow)
 {
 	SystemClass* System;
 	bool result;
